0fff77771133217afc621c27e370de673403b703
ratasorin <ratasorin0@gmail.com>
2022-01-17 00:06:25 +0200

    Self made tooltip removed

--- client/lib/popupPosition.ts
+++
@@ -1,7 +0,0 @@
-import { Subject } from 'rxjs';
-
-const popupProps = new Subject<
-  [number, number, string, 'hidden' | 'visible']
->();
-
-export default popupProps;
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,11 +2,10 @@ import { AppProps } from 'next/app';
 import '../styles.css';
 import modalStyle from './modal.module.css';
 import Modal from '../components/ModifyModal/Modal';
-import Popup from 'components/Popup/Popup';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className={modalStyle.conatiner}>
-      <Popup message="" />
       <div className={modalStyle.modal__container}>
         <Modal />
       </div>
