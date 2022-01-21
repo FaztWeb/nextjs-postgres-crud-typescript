8ef6068167dfc025df6382fe8122abb7dfd8b75b
ratasorin <ratasorin0@gmail.com>
2022-01-21 13:58:28 +0200

    Create test endpoint

--- client/pages/api/index.ts
+++ client/pages/api/index.ts
@@ -1,10 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  result: string;
-};
+interface Church {
+  info: string;
+  name: string;
+  description: string;
+}
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   console.log(req.body);
-  res.status(200).json({ result: "BESERK" })
+  res.status(200).send("SUCCESS");
 };
