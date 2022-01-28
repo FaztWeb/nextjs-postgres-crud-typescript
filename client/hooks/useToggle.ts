fe7a63ad78d240de8819dd433e5182c8d9a9b838
ratasorin <ratasorin0@gmail.com>
2022-01-29 01:26:44 +0200

    Add a toggle hook to extract the logic behind every toogle interaction that happens inside a component.

---
+++ client/hooks/useToggle.ts
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+interface Toggle {
+  state: boolean;
+  toggle: () => void;
+}
+
+const useToggle = (initialState = false): Toggle => {
+  const [state, setState] = useState<boolean>(initialState);
+  const toggle = () => {
+    setState(!state);
+    console.log(state);
+  };
+  return { state, toggle };
+};
+
+export default useToggle;
