4d785127dbe269f32f676e2d3beae58b764fd622
ratasorin <ratasorin0@gmail.com>
2022-02-09 01:18:43 +0200

    Prevent memory leaks due to timers being executed asynchronous

--- client/components/ClickOverlay/ClickOverlay.tsx
+++ client/components/ClickOverlay/ClickOverlay.tsx
@@ -1,22 +1,27 @@
 import { debounceTime, fromEvent, Subscription, tap } from 'rxjs';
 import { FC, useEffect, useRef, useState } from 'react';
 import click_style from './clickoverlay.module.css';
+import { securePipe } from 'lib/safeObservable';
+import { modal$ } from 'lib/modal';
+
 const ClickOverlay: FC = ({ children }) => {
   const buttonRef = useRef<HTMLDivElement>(null);
   const [overlays, setOverlays] = useState<JSX.Element[]>([]);
   useEffect(() => {
-    let subscription: Subscription;
+    let subscription: Subscription | undefined;
+    const { piper, stop } = securePipe(
+      debounceTime(2000),
+      tap(() => {
+        setOverlays([]);
+      })
+    );
     if (buttonRef.current)
       subscription = fromEvent(buttonRef.current, 'click')
-        .pipe(
-          debounceTime(2000),
-          tap(() => {
-            setOverlays([]);
-          })
-        )
+        .pipe(piper)
         .subscribe();
     return () => {
-      subscription.unsubscribe();
+      stop.next(true);
+      subscription ? subscription.unsubscribe() : 0;
     };
   }, []);
 
@@ -24,6 +29,10 @@ const ClickOverlay: FC = ({ children }) => {
 
   return (
     <div
+      onClick={() => {
+        modal$.next(true);
+      }}
+      ref={buttonRef}
       className={`${click_style.main__container}`}
       onMouseDown={() => {
         setOverlays([...overlays, overlayTemplate]);
