62815b4c2d13872eddd6fa6174b60addb38d9bd3
ratasorin <ratasorin0@gmail.com>
2022-02-10 15:48:04 +0200

    Include reducers for the modify modal and the info modal

--- client/store.ts
+++ client/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { buttonReducer } from 'components/ModifyModal/Button/button-slice';
+import { buttonReducer } from 'components/Widgets/Modals/Modify/Button/button-slice';
 import showSearchReducer from 'components/Searchbox/search-slice';
+import modifyModalReducer from 'components/Widgets/Modals/Modify/modify-slice';
+import infoModalReducer from 'components/Widgets/Modals/Info/info-modal-slice';
+
 export const store = configureStore({
   reducer: {
     button: buttonReducer,
     showSearch: showSearchReducer,
+    infoModal: infoModalReducer,
+    modifyModal: modifyModalReducer,
   },
 });
 
